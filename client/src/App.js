27840c5dbad3bce6ad7ebaeb799c6821cfdf728d
tomasciar <tomasciarlariello@gmail.com>
2022-07-02 06:42:22 -0400

    Move test out of main files

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,13 @@
 import logo from "./images/logo.svg";
 import "./styles/App.css";
-import { getDomain } from "./utils/setup.js";
+import Test from "./components/Test";
 
 function App() {
-  fetch(`${getDomain()}/test`).then(function (response) {
-    console.log(response.json());
-  });
-
   return (
     <div className='App'>
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
+        <Test />
       </header>
     </div>
   );
---
+++ client/src/components/Test.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import { getDomain } from "../utils/setup.js";
+
+/**
+ * Test component.
+ * @returns {JSX.Element}
+ */
+const Test = () => {
+  const [message, setMessage] = useState("");
+
+  fetch(`${getDomain()}/test`)
+    .then(response => response.json())
+    .then(data => setMessage(data.message))
+    .catch(error => console.log(error));
+
+  return <span>{message}</span>;
+};
+
+export default Test;
