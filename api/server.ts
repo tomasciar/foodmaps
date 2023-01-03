d8fb7d365b309611924856e889e345141becc5e1
Tommaso Ciarlariello <tomasciarlariello@gmail.com>
2023-01-02 22:48:05 -0500

    add testing

--- api/server.ts
+++ api/server.ts
@@ -1,6 +1,13 @@
 import express from 'express';
 import cors from 'cors';
 import env from 'dotenv';
+import { MongoClient } from 'mongodb';
+
+// For testing {
+import RestaurantData from './controllers/restaurantData.controller';
+import UberEatsScraper from './controllers/restaurant_scrapers/uberEatsScraper.controller';
+
+// }
 
 // Import routes
 import test from './routes/test.route';
@@ -15,5 +22,30 @@ app.use(cors({ origin: '*' }));
 
 // Use routes
 app.use('/test', test);
+
+const uri: string = process.env.MONGO_URI;
+const client: MongoClient = new MongoClient(uri);
+
+// For testing {
+const testMain = async () => {
+  try {
+    await client.connect();
+
+    // Testing body
+    const rd = new RestaurantData();
+    await rd.getRestaurantData(client);
+
+    const ues = new UberEatsScraper(client, ['https://www.ubereats.com/ca/category/waterloo-on/breakfast-and-brunch']);
+    const result = await ues.scrape();
+    console.log(result);
+  } catch (e: unknown) {
+    console.error(e);
+  } finally {
+    client.close();
+  }
+};
+
+testMain();
+// }
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
