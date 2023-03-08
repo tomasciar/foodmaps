4e3da4ef8d973d3f68c3e969c9f7a77a720cd2d5
tomasciar <tomasciarlariello@gmail.com>
2023-03-07 22:54:44 -0500

    add sandbox and testing scripts

--- server/server.ts
+++ server/server.ts
@@ -3,12 +3,6 @@ import cors from 'cors';
 import env from 'dotenv';
 import { MongoClient } from 'mongodb';
 
-// For testing {
-import RestaurantData from './controllers/restaurantData.controller';
-import UberEatsScraper from './controllers/restaurant_scrapers/uberEatsScraper.controller';
-
-// }
-
 // Import routes
 import test from './routes/test.route';
 
@@ -25,27 +19,5 @@ app.use('/test', test);
 
 const uri: string = process.env.MONGO_URI;
 const client: MongoClient = new MongoClient(uri);
-
-// For testing {
-const testMain = async () => {
-  try {
-    await client.connect();
-
-    // Testing body
-    const rd = new RestaurantData();
-    await rd.getRestaurantData(client);
-
-    const ues = new UberEatsScraper(client, ['https://www.ubereats.com/ca/category/waterloo-on/breakfast-and-brunch']);
-    const result = await ues.scrape();
-    console.log(result);
-  } catch (e: unknown) {
-    console.error(e);
-  } finally {
-    client.close();
-  }
-};
-
-testMain();
-// }
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
---
+++ server/tests/sandbox.ts
@@ -0,0 +1,51 @@
+import express from 'express';
+import cors from 'cors';
+import env from 'dotenv';
+import { MongoClient } from 'mongodb';
+
+// For testing {
+import RestaurantData from '../controllers/restaurantData.controller';
+import UberEatsScraper from '../controllers/restaurant_scrapers/uberEatsScraper.controller';
+
+// }
+
+// Import routes
+import test from '../routes/test.route';
+
+const app = express();
+
+env.config();
+const port = process.env.TEST_PORT || 2820;
+
+// Bypass CORS policy
+app.use(cors({ origin: '*' }));
+
+// Use routes
+app.use('/test', test);
+
+const uri: string = process.env.MONGO_URI;
+const client: MongoClient = new MongoClient(uri);
+
+// For testing {
+const testMain = async () => {
+  try {
+    await client.connect();
+
+    // Testing body
+    const rd = new RestaurantData();
+    await rd.getRestaurantData(client);
+
+    const ues = new UberEatsScraper(client, ['https://www.ubereats.com/ca/category/waterloo-on/breakfast-and-brunch']);
+    const result = await ues.scrape();
+    console.log(result);
+  } catch (e: unknown) {
+    console.error(e);
+  } finally {
+    client.close();
+  }
+};
+
+testMain();
+// }
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
