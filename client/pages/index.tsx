3017092bc854fc919124d5bd0a83930855c72948
tomasciar <tomasciarlariello@gmail.com>
2023-03-19 19:06:55 -0400

    cleanup

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -13,7 +13,7 @@ export default function Home() {
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <h1>Hello, World!</h1>
+      <main></main>
     </>
   );
 }
