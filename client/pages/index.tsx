070255bc20b2dae4a03680e80870920c3c54dd7f
tomasciar <tomasciarlariello@gmail.com>
2023-03-20 14:08:48 -0400

    update packages

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -24,7 +24,12 @@ export default function Home() {
         <h1 style={{ color: 'grey' }}>Enter an address:</h1>
         <GooglePlacesAutocomplete
           selectProps={{
-            placeholder: 'Placeholder'
+            placeholder: 'Placeholder',
+            onChange: (address: string) => {
+              // we want to send this address to the backend and turn it into a geolocation,
+              // then we want to find all the addresses that are in the geolocation area
+              // order geolocation areas by closest and then paginate through that list
+            }
           }}
           apiKey='AIzaSyBu_Gfu-PgU-rRJ7hYP2-0K9Vb3qxqGt0s'
         />
