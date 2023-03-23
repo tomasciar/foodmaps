10ff8edeca2e3ae2a118297b2befc8d2d5081486
tomasciar <tomasciarlariello@gmail.com>
2023-03-23 12:33:38 -0400

    get geolocation from address and add env vars

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import Head from 'next/head';
 import Test from '../tests/Test';
 import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
+import { getDomain } from '../utils/helpers';
 
 /**
  * Landing page
  * @notes https://tintef.github.io/react-google-places-autocomplete/docs/v2-to-v3/
  * @returns {JSX.Element}
  */
-export default function Home() {
+export default function Home(): JSX.Element {
+  const API_KEY = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY;
+
   return (
     <>
       <Head>
@@ -25,14 +28,26 @@ export default function Home() {
         <h1 style={{ color: 'grey' }}>Enter an address:</h1>
         <GooglePlacesAutocomplete
           selectProps={{
-            placeholder: 'Placeholder',
-            onChange: (address: string) => {
-              // we want to send this address to the backend and turn it into a geolocation,
-              // then we want to find all the addresses that are in the geolocation area
-              // order geolocation areas by closest and then paginate through that list
+            placeholder: 'Enter an address...',
+            onChange: async (address: any) => {
+              const response = await fetch(
+                `https://maps.googleapis.com/maps/api/geocode/json?address=${address.label}&key=${API_KEY}`
+              );
+              const data = await response.json();
+              let geolocation: any = data.results[0].geometry.location;
+              console.log(geolocation);
+
+              // ! Fix this later, right now it is not working because of the sphere index on MongoDB
+              // const response2 = await fetch(
+              //   `${getDomain()}/menu/getMenuItemsByDistance?numberOfItems=50&latitude=${geolocation.lat}&longitude=${
+              //     geolocation.lng
+              //   }`
+              // );
+              // const data2 = await response.json();
+              // console.log(data2);
             }
           }}
-          apiKey='AIzaSyBu_Gfu-PgU-rRJ7hYP2-0K9Vb3qxqGt0s'
+          apiKey={API_KEY}
         />
       </main>
     </>
