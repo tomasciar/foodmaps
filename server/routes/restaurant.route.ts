6e421cd010100e1afb685c74774dc98e36f43cdd
tomasciar <tomasciarlariello@gmail.com>
2023-03-23 12:34:28 -0400

    update routes

--- server/routes/restaurant.route.ts
+++ server/routes/restaurant.route.ts
@@ -4,5 +4,6 @@ import RestaurantController from '../controllers/restaurant.controller';
 const router = express.Router();
 
 router.route('/getRestaurants').get(RestaurantController.getRestaurants);
+router.route('/getRestaurantsItemsByDistance').get(RestaurantController.getRestaurantsByDistance);
 
 export default router;
