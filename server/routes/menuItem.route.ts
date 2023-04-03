20cc9e9fcc8b9c330f54511330291adaa237eaae
tomasciar <tomasciarlariello@gmail.com>
2023-04-02 21:22:05 -0400

    update routes

--- server/routes/menuItem.route.ts
+++ server/routes/menuItem.route.ts
@@ -6,5 +6,6 @@ const router = express.Router();
 router.route('/getMenuItems').get(MenuItemController.getMenuItems);
 router.route('/getMenuItemsByDistance').get(MenuItemController.getMenuItemsByDistance);
 router.route('/getMenuItemByUrl').get(MenuItemController.getMenuItemByUrl);
+router.route('/getMenuItemsByName').get(MenuItemController.getMenuItemsByName);
 
 export default router;
