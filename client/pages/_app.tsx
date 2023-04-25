9318adc860003a076881c0a38d81126c04fe4c11
tomasciar <tomasciarlariello@gmail.com>
2023-04-25 11:02:52 -0400

    add vercel analytics

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Navbar from '../src/components/Navbar';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Navbar />
       <Component {...pageProps} />
+      <Analytics />
     </>
   );
 }
